51f8345c95c7309c22acd743cf96889dfa8288fb
Syed Sohailur Rahman <sohailurrahman707@gmail.com>
2025-01-17 21:15:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#222';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
